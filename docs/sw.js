a9cea15a7f698e430646660ab9b3f88e90ecdf18
goudbas <bas_goudriaan@hotmail.com>
2026-09-13 21:12:28 +0200

    Rename Today's log-navigatieknop naar "Logs" voor duidelijkheid

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rx-dev-v25';
+const CACHE = 'rx-dev-v26';
 const SHELL = ['./', './index.html', './decision-engine.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (e) => {
